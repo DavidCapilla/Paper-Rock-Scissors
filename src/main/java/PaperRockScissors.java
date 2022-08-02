ca4d5672bed6ff73648b4bf3a5ff470763f2b5cb
David Capilla <dcapillag@gmail.com>
2022-08-02 19:52:46 +0200

    dcg | Add input

--- src/main/java/PaperRockScissors.java
+++ src/main/java/PaperRockScissors.java
@@ -1,11 +1,19 @@
+import java.util.Scanner;
+
 public class PaperRockScissors {
 
   private PaperRockScissorsRound paperRockScissorsRound = new PaperRockScissorsRound();
 
   public void playGame(int numberOfRounds) {
 
+    Scanner inputScanner = new Scanner(System.in);
     for (int round = 0; round < numberOfRounds; round++) {
-      paperRockScissorsRound.playRound(HandSign.SCISSORS, HandSign.SCISSORS);
+      String playerOneSign = inputScanner.next();
+      paperRockScissorsRound.playRound(convertStringToSign(playerOneSign), HandSign.SCISSORS);
     }
+  }
+
+  private HandSign convertStringToSign(String playerOneSign) {
+    return HandSign.SCISSORS;
   }
 }
--- src/test/java/PaperRockScissorsTest.java
+++ src/test/java/PaperRockScissorsTest.java
@@ -1,3 +1,5 @@
+import java.io.ByteArrayInputStream;
+import java.util.Scanner;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -16,6 +18,8 @@ class PaperRockScissorsTest {
   @ParameterizedTest
   @CsvSource({"0", "1", "2", "20"})
   public void playGame_callsNTimesToPlayRound(int numberOfRounds) {
+
+    System.setIn(new ByteArrayInputStream("rock\n".repeat(numberOfRounds).getBytes()));
     testee.playGame(numberOfRounds);
     Mockito.verify(paperRockScissorsRound, Mockito.times(numberOfRounds))
         .playRound(ArgumentMatchers.any(), ArgumentMatchers.any());
