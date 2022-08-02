2a82761ea0614052fc2f2414eb88aaffa591274e
David Capilla <dcapillag@gmail.com>
2022-08-02 20:40:51 +0200

    dcg | Add game functionality

--- src/main/java/PaperRockScissors.java
+++ src/main/java/PaperRockScissors.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class PaperRockScissors {
@@ -8,12 +9,40 @@ public class PaperRockScissors {
 
     Scanner inputScanner = new Scanner(System.in);
     for (int round = 0; round < numberOfRounds; round++) {
-      String playerOneSign = inputScanner.next();
-      paperRockScissorsRound.playRound(convertStringToSign(playerOneSign), HandSign.SCISSORS);
+      HandSign playerOneHandSign = getHandSignFromInput(inputScanner);
+      HandSign playerTwoHandSign = getRandomHandSign();
+      RoundResult roundResult = paperRockScissorsRound.playRound(playerOneHandSign, playerTwoHandSign);
+
+      System.out.println("Player One throws " + playerOneHandSign +
+          " and Player Two throws throws " + playerTwoHandSign +
+          ". " + roundResult);
     }
   }
 
+  private HandSign getHandSignFromInput(Scanner inputScanner) {
+
+    HandSign playerOneHandSign = null;
+    while (playerOneHandSign == null) {
+      System.out.println("Introduce the hand sign you want to play (paper, rock or scissors): ");
+      String playerOneSign = inputScanner.next();
+      playerOneHandSign = convertStringToSign(playerOneSign);
+      if (playerOneHandSign == null){
+        System.out.println("Invalid sign " + playerOneSign + ". Introduce a valid sign: paper, rock or scissors");
+      }
+    }
+    return playerOneHandSign;
+  }
+
   private HandSign convertStringToSign(String playerOneSign) {
-    return HandSign.SCISSORS;
+    return switch (playerOneSign.toUpperCase()) {
+      case "PAPER" -> HandSign.PAPER;
+      case "ROCK" -> HandSign.ROCK;
+      case "SCISSORS" -> HandSign.SCISSORS;
+      default -> null;
+    };
+  }
+
+  private HandSign getRandomHandSign() {
+    return HandSign.values()[new Random().nextInt(HandSign.values().length)];
   }
 }
