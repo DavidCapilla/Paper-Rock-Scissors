5bd68112dd665e8d5ac37984b70288e3b17b34cb
David Capilla <dcapillag@gmail.com>
2022-08-02 18:41:21 +0200

    dcg | Fix test adding playRound functionality

--- src/main/java/PaperRockScissors.java
+++ src/main/java/PaperRockScissors.java
@@ -2,6 +2,20 @@ public class PaperRockScissors {
 
   public RoundResult playRound(HandSign playerOneSign, HandSign playerTwoSign) {
 
-    return RoundResult.DRAW;
+    if (playerOneSign.equals(playerTwoSign)) {
+      return RoundResult.DRAW;
+    } else if (getSignThatBeats(playerOneSign).equals(playerTwoSign)) {
+      return RoundResult.WINS_PLAYER_ONE;
+    }
+    return RoundResult.WINS_PLAYER_TWO;
+  }
+
+  private HandSign getSignThatBeats(HandSign handSign) {
+
+    return switch (handSign) {
+      case PAPER -> HandSign.ROCK;
+      case ROCK -> HandSign.SCISSORS;
+      case SCISSORS -> HandSign.PAPER;
+    };
   }
 }
